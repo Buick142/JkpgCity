a3350e9c84999a7dbe49977e7e2d903e1b10d365
Noah Buchanan <94912301+Buick142@users.noreply.github.com>
2025-03-10 13:46:31 +0100

    running on localhost:3000

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const db = require('./database.js')
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Server is running. Welcome to JkpgCity!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 }); 
