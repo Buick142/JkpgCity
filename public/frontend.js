26594b931ae20230ae4b7e27962486ed3b24e7c6
Noah Buchanan <94912301+Buick142@users.noreply.github.com>
2025-03-10 15:35:28 +0100

    trying to fetch from database

--- public/frontend.js
+++ public/frontend.js
@@ -1,3 +1,26 @@
 import { getAllStores } from './database.js';
 
 console.log(getAllStores());
+
+document.addEventListener('DOMContentLoaded', async function () {
+    const container = document.getElementById('store-container');
+
+    try {
+        const response = await fetch('/api/stores');
+        const stores = await response.json();
+
+        stores.forEach(store => {
+            const card = document.createElement('div');
+            card.classList.add('store-card'); 
+            card.innerHTML =
+                <h2>${store.name}</h2>
+                <p>${store.description}</p>
+                <p><strong>Location</strong>${store.location}</p>
+            ;
+        
+        container.appendChild(card);
+        });
+    } catch (error) {
+        console.error('Error fetching stores from database:', error);
+    }
+});
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const db = require('./database.js')
-
+const db = require('./database.js');
+const { getAllStores } = require('./database.js');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
 
+app.get('/api/stores', (req, res) => {
+    const stores = getAllStores();
+    res.json(stores);
+})
+
 app.get('/', (req, res) => {
     res.send('Server is running. Welcome to JkpgCity!');
 });
+
+app.get('/favicon.ico', (req, res) => res.status(204)); // Ignore Favicon request (Provided by ChatGPT)
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
